f537c552e0942490e61af80e24c3b9f8b8684a29
Дмитрий Кощеев <barada.90@yandex.ru>
2023-01-25 22:36:13 +0300

    Перегрузка_операторов

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ using namespace std;
 //#define STRUCT_POINT 
 //#define CONSTRUCTOR_CHECK
 //#define ASSIGMENT_CHECK_1
-#define ASSIGMENT_CHECK_2 
+//#define ASSIGMENT_CHECK_2 
+#define OVERLOAD_OPERATORS_1
 // 
 //1. В классе Point написать get / set - методы;
 //2. В классе Point написать метод ? ? ? distance(? ? ? ), 
@@ -59,12 +60,26 @@ public:
 		cout << "Destructor:\t " << this << endl;
 	}
 	//Operators:
-	Point operator =(const Point& other)
+	
+	Point& operator =(const Point& other)
 	{
 		this->x = other.x;
 		this->y = other.y;
 		cout << "CopyAssignment:\t" << this << endl;
 		return *this;
+	}
+	Point& operator ++()
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point& operator ++(int)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
 	}
 
 
@@ -90,6 +105,14 @@ double distance(const Point& A,const Point& B)
 	double y_distanse = A.get_y() - B.get_y();
 	double distanse = sqrt(x_distanse * x_distanse + y_distanse * y_distanse);
 	return distanse;
+}
+Point operator+(const Point& left, const Point& right)
+{
+	Point res;
+	res.set_x(left.get_x() + right.get_x());
+	res.set_y(left.get_y() + right.get_y());
+	return res;
+	
 }
 void main()
 {
@@ -142,10 +165,27 @@ void main()
 #ifdef ASSIGMENT_CHECK_2
 	int a, b, c;
 	a = b = c = 0;
+	cout << delimetr << endl;
 	Point A, B, C;
+	cout << delimetr << endl;
 	A = B =  C = Point(2, 3);
+	cout << delimetr << endl;
 
 
 #endif // ASSIGMENT_CHECK_2
+#ifdef  OVERLOAD_OPERATORS_1
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	Point A(2, 3);
+	Point B(4, 5);
+	Point C = A + B;
+	C.print();
+	++C;
+	C.print();
+	C++;
+	C.print();
+#endif //  OVERLOAD_OPERATORS_1
 
 }
