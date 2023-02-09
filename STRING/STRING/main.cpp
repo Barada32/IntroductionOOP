5c0d579f6e918339710bf1a89afb1161d3fef584
Дмитрий Кощеев <barada.90@yandex.ru>
2023-02-09 21:02:58 +0300

    HeaderInitDone

--- STRING/STRING/main.cpp
+++ STRING/STRING/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-////#define BASE_CHECK
-
+#define BASE_CHECK
+//#define CALLING_CONSTRUCTOR
 class String
 {
 	int size;		//размер строки
@@ -21,35 +21,25 @@ public:
 	}
 
 	//				Constructors:
-	explicit String(int size = 80)
+	explicit String(int size = 80) :size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
 		cout << "DefConstructor:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str) :size(strlen(str)+1),str(new char[size]{})
 	{
-		this->size = strlen(str) + 1;//функция strlen() возвращает размер строки
-
-		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "1ArgConstructor: \t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other): size(other.size),str(new char[size]{})
 	{
 		//Deep copy - побитовое копирование
-		this->size = other.size;
-		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 			this->str[i] = other.str[i];
 		cout << "CopyConstructor:\t" << endl;
 	}
-	String(String&& other)noexcept
+	String(String&& other)noexcept:size(other.size),str(other.str)
 	{
 		//Shallow Copy - поверхностное копирование
-
-		this->size = other.size;
-		this->str = other.str;
 		other.size = 0;
 		other.str = nullptr;
 		cout << "MoveConstructor: " << this << endl;
@@ -117,36 +107,39 @@ void main()
 {
 	setlocale(LC_ALL, "");
 
-	/*String str1();
-	str1.print();*/
+	
 #ifdef BASE_CHECK
+	String str1(5);
+	str1.print();
 
-	String str1 = "Hello";//Hello строковая константа
+	str1 = "Hello";	//"Hello" - это строковая константа
+	str1 = str1;
 	cout << str1 << endl;
+
 	String str2 = "World";
 	cout << str2 << endl;
-	String str3 = str1 + str2;
-	cout << str3 << endl;
+	String str4 = str2;
+	str4.print();
 #endif // BASE_CHECK
-
-	String str1;	//Default constructor
-	str1.print();
-	String str2 = "Hello";	//Single argument constructor
-	str2.print();
-	String str3 = str2;	//Copy constructor
-	str3.print();
-	String str4();	//Здесь не вызывается никакой конструктор, потому что НЕ создается объект,
-	//Здесь объявлется функция str4(), которая не принимает никаких параметров,
-	//и возвращает значение типа String.
-//str4.print();	//str4 - это НЕ объект, это функция, а для функции нельзя вызвать метод.
-//Если мы хотим явно вызвать конструктор по умолчанию, то это можно сделать так:
-	String str5{};	//Явный вызов конструктора по умолчанию.
-	str5.print();
-	String str6{ 22 };
-	str6.print();
-	String str7{ "World" };
-	str7.print();
-	String str8{ str7 };	//Copy constructor
+//
+//	String str1;	//Default constructor
+//	str1.print();
+//	String str2 = "Hello";	//Single argument constructor
+//	str2.print();
+//	String str3 = str2;	//Copy constructor
+//	str3.print();
+//	String str4();	//Здесь не вызывается никакой конструктор, потому что НЕ создается объект,
+//	//Здесь объявлется функция str4(), которая не принимает никаких параметров,
+//	//и возвращает значение типа String.
+////str4.print();	//str4 - это НЕ объект, это функция, а для функции нельзя вызвать метод.
+////Если мы хотим явно вызвать конструктор по умолчанию, то это можно сделать так:
+//	String str5{};	//Явный вызов конструктора по умолчанию.
+//	str5.print();
+//	String str6{ 22 };
+//	str6.print();
+//	String str7{ "World" };
+//	str7.print();
+//	String str8{ str7 };	//Copy constructor
 
 
 }
