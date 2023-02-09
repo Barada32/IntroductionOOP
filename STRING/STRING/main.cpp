b4f4ef69379de3284324442ec4813eac700d5ea6
Дмитрий Кощеев <barada.90@yandex.ru>
2023-02-09 20:39:34 +0300

    11

--- STRING/STRING/main.cpp
+++ STRING/STRING/main.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+////#define BASE_CHECK
 
 class String
 {
@@ -46,7 +47,7 @@ public:
 	String(String&& other)noexcept
 	{
 		//Shallow Copy - поверхностное копирование
-		
+
 		this->size = other.size;
 		this->str = other.str;
 		other.size = 0;
@@ -85,7 +86,7 @@ public:
 		return *this;
 	}
 
-	
+
 	//				Methods:
 	void print()const
 	{
@@ -115,12 +116,37 @@ std::ostream& operator<<(std::ostream& os, const String& obj)
 void main()
 {
 	setlocale(LC_ALL, "");
+
 	/*String str1();
 	str1.print();*/
+#ifdef BASE_CHECK
+
 	String str1 = "Hello";//Hello строковая константа
 	cout << str1 << endl;
 	String str2 = "World";
 	cout << str2 << endl;
 	String str3 = str1 + str2;
 	cout << str3 << endl;
+#endif // BASE_CHECK
+
+	String str1;	//Default constructor
+	str1.print();
+	String str2 = "Hello";	//Single argument constructor
+	str2.print();
+	String str3 = str2;	//Copy constructor
+	str3.print();
+	String str4();	//Здесь не вызывается никакой конструктор, потому что НЕ создается объект,
+	//Здесь объявлется функция str4(), которая не принимает никаких параметров,
+	//и возвращает значение типа String.
+//str4.print();	//str4 - это НЕ объект, это функция, а для функции нельзя вызвать метод.
+//Если мы хотим явно вызвать конструктор по умолчанию, то это можно сделать так:
+	String str5{};	//Явный вызов конструктора по умолчанию.
+	str5.print();
+	String str6{ 22 };
+	str6.print();
+	String str7{ "World" };
+	str7.print();
+	String str8{ str7 };	//Copy constructor
+
+
 }
